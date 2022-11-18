58a5cac0901da9fb811d221c784508237abc93e8
jasonltorchinsky <jason.torchinsky@wisc.edu>
2022-11-18 17:18:32 -0600

    Completes draft of interior convection matrix construction.

--- src/dg/quadrature/__init__.py
+++ src/dg/quadrature/__init__.py
@@ -1,6 +1,6 @@
 from .face_2_face import face_2_face
 from .face_2_dface import face_2_dface
-from .gl_ddx import gl_ddx
+from .gl_ddx import gl_ddx, gl_deriv
 from .gl_eval import gl_eval
 from .gl_proj import gl_proj
 from .gl_proj_2D import gl_proj_2D 
--- src/rad_amr/rtdg_amr.py
+++ src/rad_amr/rtdg_amr.py
@@ -76,6 +76,8 @@ def calc_mass_matrix(mesh, kappa):
                     [dof_a] = cell.ndofs
 
                     # Indexing from i, j, a to beta
+                    # In this case, the alpha and beta indices are the same,
+                    # so we don't have to do them separately
                     def beta(ii, jj, aa):
                         val = dof_a * dof_y * ii \
                             + dof_a * jj \
@@ -233,3 +235,104 @@ def calc_scat_matrix(mesh, sigma, phi):
     M_scat = block_diag(col_mtxs, format = 'csr')
 
     return M_scat
+
+def calc_int_scat_matrix(mesh):
+
+    # Store local-colmun matrices in here
+    ncols = len(mesh.cols.keys())
+    col_mtxs = ncols * [0]
+    col_idx = 0
+
+    for col_key, col in sorted(mesh.cols.items()):
+        if col.is_lf:
+            # Get column information
+            [x0, y0, x1, y1] = col.pos
+            dx = x1 - x0
+            dy = y1 - y0
+            [dof_x, dof_y] = col.ndofs
+            
+            [nodes_x, weights_x, nodes_y, weights_y, _, _] = qd.quad_xya(dof_x, dof_y, 1)
+            
+            # Store local-element matrices in here
+            ncells = len(list(col.cells.keys()))
+            cell_mtxs = ncells * [0]
+            cell_idx = 0
+
+            for cell_key, cell in sorted(col.cells.items()):
+                if cell.is_lf:
+                    [dof_a] = cell.ndofs
+
+                    # Indexing from i, j, a to beta
+                    def beta(ii, jj, aa):
+                        val = dof_a * dof_y * ii \
+                            + dof_a * jj \
+                            + aa
+                        return val
+                    
+                    # Get cell information
+                    cell_ndof = dof_x * dof_y * dof_a
+                    [a0, a1] = cell.pos
+                    da = a1 - a0
+                    dcoeff = dx * dy * da / 8
+                    
+                    [_, _, _, _, nodes_a, weights_a] = qd.quad_xya(1, 1, dof_a)
+                    th = a0 + (a1 - a0) / 2 * (nodes_a + 1)
+
+                    # Construct delta_ip * delta_ar term
+                    cell_mtx_ndof_ipar = dof_x * dof_y**2 * dof_a # Number of non-zero terms
+                    
+                    alphalist_ipar = np.zeros([cell_mtx_ndof_ipar],
+                                         dtype = np.int32) # alpha index
+                    betalist_ipar = np.zeros([cell_mtx_ndof_ipar],
+                                        dtype = np.int32) # beta index
+                    vlist_ipar = np.zeros([cell_mtx_ndof_ipar]) # Matrix entry
+                    cnt = 0
+
+                    for ii in range(0, dof_x):
+                        wx_i = weights_x[ii]
+                        for jj in range(0, dof_y):
+                            wy_j = weights_y[jj]
+                            for aa in range(0, dof_a):
+                                wth_a = weights_a[aa]
+                                for qq in range(0, dof_y):
+                                    betalist_ipar[cnt] = beta(ii, qq, aa)
+                                    alphalist_ipar[cnt] = beta(ii, jj, aa)
+                                    # NEED TO FIX THIS FORMULA
+                                    vlist_ipar[cnt] = dcoeff * wx_i * wy_j * wth_a \
+                                        * qd.gl_deriv(nodes_y, qq, nodes_y[jj]) * np.sin(th[aa])
+                                    cnt += 1
+
+                    # Construct delta_jq * delta_ar term
+                    cell_mtx_ndof_jqar = dof_x**2 * dof_y dof_a # Number of non-zero terms
+                    
+                    alphalist_jqar = np.zeros([cell_mtx_ndof_ipar],
+                                         dtype = np.int32) # alpha index
+                    betalist_jqar = np.zeros([cell_mtx_ndof_ipar],
+                                        dtype = np.int32) # beta index
+                    vlist_jqar = np.zeros([cell_mtx_ndof_ipar]) # Matrix entry
+                    cnt = 0
+                    
+                    for ii in range(0, dof_x):
+                        wx_i = weights_x[ii]
+                        for jj in range(0, dof_y):
+                            wy_j = weights_y[jj]
+                            for aa in range(0, dof_a):
+                                wth_a = weights_a[aa]
+                                for pp in range(0, dof_x):
+                                    betalist_jqar[cnt] = beta(pp, jj, aa)
+                                    alphalist_jqar[cnt] = beta(ii, jj, aa)
+                                    # NEED TO FIX THIS FORMULA
+                                    vlist_jqar[cnt] = dcoeff * wx_i * wy_j * wth_a \
+                                        * qd.gl_deriv(nodes_x, pp, nodes_x[ii]) * np.cos(th[aa])
+                                    cnt += 1
+                                
+                    cell_mtxs[cell_idx] = coo_matrix((vlist_ipar, (alphalist_ipar, betalist_ipar))) + coo_matrix((vlist_jqar, (alphalist_jqar, betalist_jqar)))
+                    cell_idx += 1
+                    
+            col_mtxs[col_idx] = block_diag(cell_mtxs, format = 'csr')
+            
+            col_idx += 1
+                
+    M_int_conv = block_diag(col_mtxs, format = 'csr')
+
+    return M_int_conv
